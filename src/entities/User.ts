2443e67233c4c5e48890d2a9c0845710c2fe317d
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-06-04 19:34:57 +0530

    add validation to user entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,27 +1,51 @@
-import { IsEmail } from "class-validator";
+import { IsEmail, IsPhoneNumber, Length, Max } from "class-validator";
 import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Case } from "./Case";
 import { Session } from "./Session";
+
+enum UserStatus {
+  BLOCKED = "blocked",
+  ACTIVE = "active",
+  INACTIVE = "inactive"
+}
+
+enum PreferredPayment {
+  CREDIT = "credit",
+  DEBIT = "debit",
+  UPI = "upi"
+}
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
   @Column()
+  @Max(50)
   firstName: string;
   @Column()
+  @Max(50)
   lastName: string;
   @Column()
+  @Length(1, 50)
   username: string;
   @Column()
   @IsEmail()
+  @Length(7, 254)
   email: string;
-  @Column()
+  @Column({ select: false })
   password: string;
   @Column()
+  @Length(6, 15)
+  @IsPhoneNumber('IN')
   phone: string;
-  @Column()
+  @Column({
+    type: "enum",
+    enum: PreferredPayment
+  })
   preferredPayment: string;
-  @Column()
+  @Column({
+    type: "enum",
+    enum: UserStatus
+  })
   userStatus: number;
   @OneToMany(() => Session, (session) => session.user)
   sessions: Session[];
