31906ee3d18008bf976fb5da5702a6698da6edc7
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-06-09 22:48:25 +0530

    implement create-user validator

---
+++ src/router/users/controller/validators/create-user.ts
@@ -0,0 +1,39 @@
+import Filter from 'bad-words';
+import { check } from "express-validator";
+const filter = new Filter();
+
+const validator = [
+  check('email')
+    .trim()
+    .optional()
+    .isEmail()
+    .isLength({ min: 7, max: 320 })
+    .withMessage('provide a valid email address'),
+  check('firstName')
+    .trim()
+    .not()
+    .isEmpty()
+    .isLength({ min: 1, max: 50 }),
+  check('lastName')
+    .trim()
+    .not()
+    .isEmpty()
+    .isLength({ min: 1, max: 50 }),
+  check('phone')
+    .not()
+    .isEmpty()
+    .isLength({ min: 6, max: 15 })
+    .isMobilePhone('en-IN')
+    .withMessage(
+      'provide a password with 7 letters that does not contain the word password'
+    ),
+  check('preferredPayment')
+    .optional()
+    .withMessage('provide timezone string'),
+
+];
+
+export {
+  validator as removeMemberValidator
+};
+
