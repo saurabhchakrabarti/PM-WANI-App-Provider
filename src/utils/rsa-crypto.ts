9716f6ddbfe1abd73b406e5cbee7814bda857fde
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-07-09 01:25:53 +0530

    implement rsa crypto utility for encryption and decryption

---
+++ src/utils/rsa-crypto.ts
@@ -0,0 +1,59 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+
+function encrypt(toEncrypt: string, key?: string | Buffer, relativeOrAbsolutePathToPublicKey?: string) {
+  if (!key && !relativeOrAbsolutePathToPublicKey) {
+    return null
+  }
+
+  let publicKey: string | Buffer = "";
+  if (relativeOrAbsolutePathToPublicKey) {
+
+    const absolutePath = path.resolve(relativeOrAbsolutePathToPublicKey)
+    publicKey = fs.readFileSync(absolutePath, 'utf8')
+  }
+
+  if (key) {
+    publicKey = key
+  }
+  const buffer = Buffer.from(toEncrypt, 'utf8')
+  const encrypted = crypto.publicEncrypt(publicKey, buffer)
+  return encrypted.toString('base64')
+}
+
+function decrypt(toDecrypt: string, key?: string | Buffer, relativeOrAbsolutePathToPrivateKey?: string) {
+  if (!key && !relativeOrAbsolutePathToPrivateKey) {
+    return null
+  }
+
+  let privateKey: string | Buffer = "";
+  if (relativeOrAbsolutePathToPrivateKey) {
+    const absolutePath = path.resolve(relativeOrAbsolutePathToPrivateKey)
+    privateKey = fs.readFileSync(absolutePath, 'utf8')
+  }
+
+  if (key) {
+    privateKey = key
+  }
+
+
+  const buffer = Buffer.from(toDecrypt, 'base64')
+  const decrypted = crypto.privateDecrypt(
+    {
+      key: privateKey.toString(),
+      passphrase: '',
+    },
+    buffer,
+  )
+  return decrypted.toString('utf8')
+}
+
+function getPubKeyFromCert(cert: string) {
+  return crypto.createPublicKey(cert).export({ type: 'pkcs1', format: 'pem' })
+}
+
+export {
+  encrypt, decrypt, getPubKeyFromCert
+};
+
