88e4df9850d6ddf5ac9d4c55ba774b86f1cc79b2
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-07-12 20:06:06 +0530

    add routes to user router

--- src/router/users/index.ts
+++ src/router/users/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { validateRequest } from '../../middleware/validate-request';
 import { userRoutes } from '../routes';
 import { createUserHandler } from './controller/handlers/create-user';
+import { loginUserHandler } from './controller/handlers/login-user';
 import { createUserValidator } from './controller/validators/create-user';
+import { loginUserValidator } from './controller/validators/login-user';
 const router = express.Router();
 
-router.get(userRoutes.CreateUser, createUserValidator, validateRequest, createUserHandler)
-
+router.post(userRoutes.CreateUser, createUserValidator, validateRequest, createUserHandler)
+router.post(userRoutes.LoginUser, loginUserValidator, validateRequest, loginUserHandler)
 export {
   router as userRouter
 };
