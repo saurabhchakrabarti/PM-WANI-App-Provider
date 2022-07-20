b5daebb598ce09e63bf84ade60845e9b40cb2bdd
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-07-20 21:07:13 +0530

    add public decrypt and private encrypt methods

--- src/utils/rsa-crypto.ts
+++ src/utils/rsa-crypto.ts
@@ -25,6 +25,29 @@ function encrypt(toEncrypt: string, key?: string | Buffer, relativeOrAbsolutePat
   return encrypted.toString('base64')
 }
 
+function privateEncrypt(toEncrypt: string, key?: string | Buffer, relativeOrAbsolutePathToPublicKey?: string) {
+  if (!key && !relativeOrAbsolutePathToPublicKey) {
+    return null
+  }
+
+  let publicKey: string | Buffer = "";
+  if (relativeOrAbsolutePathToPublicKey) {
+
+    const absolutePath = path.resolve(relativeOrAbsolutePathToPublicKey)
+    publicKey = fs.readFileSync(absolutePath, 'utf8')
+  }
+
+  if (key) {
+    publicKey = key
+  }
+  const buffer = Buffer.from(toEncrypt, 'utf8')
+  const encrypted = crypto.privateEncrypt({
+    key: publicKey,
+    padding: crypto.constants.RSA_PKCS1_PADDING,
+  }, buffer,)
+  return encrypted.toString('base64')
+}
+
 function decrypt(toDecrypt: string, key?: string | Buffer, relativeOrAbsolutePathToPrivateKey?: string) {
   if (!key && !relativeOrAbsolutePathToPrivateKey) {
     return null
@@ -52,11 +75,40 @@ function decrypt(toDecrypt: string, key?: string | Buffer, relativeOrAbsolutePat
   return decrypted.toString('utf8')
 }
 
+
+function publicDecrypt(toDecrypt: string, key?: string | Buffer, relativeOrAbsolutePathToPrivateKey?: string) {
+  if (!key && !relativeOrAbsolutePathToPrivateKey) {
+    return null
+  }
+
+  let privateKey: string | Buffer = "";
+  if (relativeOrAbsolutePathToPrivateKey) {
+    const absolutePath = path.resolve(relativeOrAbsolutePathToPrivateKey)
+    privateKey = fs.readFileSync(absolutePath, 'utf8')
+  }
+
+  if (key) {
+    privateKey = key
+  }
+
+
+  const buffer = Buffer.from(toDecrypt, 'base64')
+  const decrypted = crypto.publicDecrypt(
+    {
+      key: privateKey.toString(),
+      padding: crypto.constants.RSA_PKCS1_PADDING,
+    },
+    buffer,
+  )
+  return decrypted.toString('utf8')
+}
+
+
 function getPubKeyFromCert(cert: string) {
   return crypto.createPublicKey(cert).export({ type: 'pkcs1', format: 'pem' })
 }
 
 export {
-  encrypt, decrypt, getPubKeyFromCert
+  encrypt, decrypt, getPubKeyFromCert, publicDecrypt, privateEncrypt
 };
 
