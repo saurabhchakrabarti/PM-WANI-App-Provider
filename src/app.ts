5f926cf790afc9d6c1973a41bd6afc2f4d4bfb80
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-07-27 09:11:13 +0530

    add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 import express from 'express';
 import 'express-async-errors'; // to use throw in async functions
 import session from 'express-session';
 import helmet from 'helmet';
-import morgan from 'morgan';
-
 import Keycloak from "keycloak-connect";
+import morgan from 'morgan';
 import { errorHandler } from './middleware/error-handler';
 
 const app = express();
 app.use(helmet());
+app.use(cors())
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
