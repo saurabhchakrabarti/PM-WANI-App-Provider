23771e82d3d7d73b3b328b3690014b8ae20c35e3
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-07-29 02:31:10 +0530

    shift keycloak create user logic to user model

--- src/router/users/controller/handlers/create-user.ts
+++ src/router/users/controller/handlers/create-user.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { kcAdminClient } from '../../../../keycloak/keycloak-admin-client';
 import { User } from '../../../../models/User';
 
 const handler = async (req: Request, res: Response) => {
@@ -8,26 +7,6 @@ const handler = async (req: Request, res: Response) => {
   const { firstName, lastName, username, email, password, phone, preferredPayment } = req.body;
 
   // TODO check if user with given credentials exists and then create the user
-  await kcAdminClient.users.create({
-    username,
-    email,
-    firstName,
-    lastName,
-    credentials: [{
-      type: "password",
-      value: password,
-      temporary: false
-    }],
-    attributes: {
-      phone,
-      preferredPayment
-    },
-    // enabled required to be true in order to send actions email
-    emailVerified: true,
-    enabled: true,
-    realmRoles: ['app-user']
-  });
-
 
   const userId = await User.createUser({
     firstName, lastName, username, email, password, phone, preferredPayment
