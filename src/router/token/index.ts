7c9571f1d2574c610bc421cbdfe1912e5824ae17
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-07-29 03:57:28 +0530

    add keycloak protect to passWaniToken

--- src/router/token/index.ts
+++ src/router/token/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { keycloak } from '../../app';
 import { validateRequest } from '../../middleware/validate-request';
 import { tokenRoutes } from '../routes';
 import { passWaniAppTokenHandler } from './controller/handler/pass-waniapptoken';
@@ -9,7 +10,7 @@ import { verifyWaniPdoaValidator } from './controller/validator/verify-wani-pdoa
 const router = express.Router();
 
 router.get(tokenRoutes.VerifyWaniPdoa, verifyWaniPdoaValidator, validateRequest, verifyWaniPdoaHandler)
-router.get(tokenRoutes.passWaniapptoken, passWaniapptokenValidator, validateRequest, passWaniAppTokenHandler)
+router.get(tokenRoutes.passWaniapptoken, keycloak.protect(), passWaniapptokenValidator, validateRequest, passWaniAppTokenHandler)
 
 export {
   router as tokenRouter
